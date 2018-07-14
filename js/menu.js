6675a28d1e137add50a78b7f06ca17de1142cf8d
parkjinok <jwew2@naver.com>
2018-07-14 13:32:55 +0900

    esc키를 이요안 그리드 토글 기능 추가

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,12 @@
-$(document).keyup(function(e){
-  if(e.keyCode === 27){
-    $('.grid').toggleClass('grid-show');
-  }
-});
+// $(document).ready(function(){
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      $('.grid').toggleClass('grid-show');
+    }
+  });
+// });
+
+
+
+
+
