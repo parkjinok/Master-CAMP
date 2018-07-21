0518aadeaa78cad0f5dacb17d028c9af103ae746
parkjinok <jwew2@naver.com>
2018-07-21 15:25:36 +0900

    btn-menu-act 클래스 토글 기능

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
-// $(document).ready(function(){
-  $(document).keyup(function (e) {
-    if (e.keyCode === 27) {
-      $('.grid').toggleClass('grid-show');
-    }
-  });
-// });
+// // $(document).ready(function(){
+//   $(document).keyup(function (e) {
+//     if (e.keyCode === 27) {
+//       $('.grid').toggleClass('grid-show');
+//     }
+//   });
+// // });
 
+var btn = $('.btn-menu');
 
-
-
+btn.click(function(){
+  $(this).toggleClass('btn-menu-act');
+});
 
